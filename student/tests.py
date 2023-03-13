d8593d55100bd5a003bf1aa6ae74b64d8f206af2
DHRUV <dhruvsp.inexture@gmail.com>
2023-03-13 16:43:19 +0530

    Added test cases for students.

--- student/tests.py
+++ student/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from tests.test_setup import TestSetUp
+
+
+class TestStudent(TestSetUp):
+    def test_enroll_course_success(self):
+        self.add_course()
+        self.get_logged_in_student()
+        enroll_course_data = {"course": 1}
+        response = self.client.post(reverse('enroll-course'), enroll_course_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['student'], 2)
+        self.assertEqual(response.data['course'], 1)
+
+    def test_get_enrolled_course(self):
+        self.test_enroll_course_success()
+        response = self.client.get(reverse('enroll-course'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['course']['name'], 'test course')
+
+
