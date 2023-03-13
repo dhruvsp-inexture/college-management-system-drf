97e54f65893b48955927a8bed93f47c5cbb6ed82
DHRUV <dhruvsp.inexture@gmail.com>
2023-03-13 11:59:27 +0530

    Fix validations while creating and updating course and also change model for the same.

--- courses/models.py
+++ courses/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=500)
-    start_date = models.DateField(default=date.today())
+    start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
 
--- courses/serializers.py
+++ courses/serializers.py
@@ -5,9 +5,11 @@ from dateutil.relativedelta import relativedelta
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    start_date = serializers.DateField(default=date.today())
+
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'start_date', 'end_date']
+        fields = '__all__'
 
     def validate_start_date(self, value):
         if value < date.today():
@@ -16,28 +18,33 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # If end_date was not provided, set it to 3 months after start_date
-        if 'end_date' not in validated_data:
-            start_date = validated_data['start_date']
+        if ('end_date' not in validated_data):
+            if 'start_date' not in validated_data:
+                start_date = self.fields['start_date'].get_default()
+            else:
+                start_date = validated_data.get('start_date')
             end_date = start_date + relativedelta(months=3)
             validated_data['end_date'] = end_date
 
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if validated_data.get('end_date'):
-            if instance.start_date >= validated_data['end_date']:
-                raise serializers.ValidationError("End Date should be after Start Date!")
-        if validated_data.get('start_date'):
-            if instance.end_date <= validated_data['start_date']:
-                raise serializers.ValidationError("Start Date should be before End Date!")
-        if validated_data.get('start_date') and validated_data.get('end_date'):
-            if validated_data['start_date'] >= validated_data['end_date']:
-                raise serializers.ValidationError("Start Date should be before End Date!")
-        if validated_data.get('start_date'):
-            instance.start_date = validated_data.get('start_date')
-        if validated_data.get('end_date'):
-            instance.end_date = validated_data.get('end_date')
+        start_date = validated_data.get('start_date')
+        end_date = validated_data.get('end_date')
 
+        if start_date and end_date and (start_date >= end_date):
+            raise serializers.ValidationError("Start Date should be before End Date!")
+
+        if start_date and (instance.end_date <= start_date):
+            raise serializers.ValidationError("Start Date should be before End Date!")
+
+        if end_date and (instance.start_date >= end_date):
+            raise serializers.ValidationError("End Date should be after Start Date!")
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.start_date = start_date or instance.start_date
+        instance.end_date = end_date or instance.end_date
         instance.save()
         return instance
 
